fff06e7f21a4c5378b1ae59b29d1d4044b69eab4
Sum-sdl <1079569023@qq.com>
2020-04-25 15:17:43 +0800

    simple test

--- app/src/main/java/com/example/gradletest/MainActivity.kt
+++ app/src/main/java/com/example/gradletest/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.gradletest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.example.gradletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var constraintLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        constraintLayout = binding.cl
+
+        binding.button2.setOnClickListener {
+
+            animateToKeyframeTwo()
+        }
+
+        binding.button3.setOnClickListener {
+
+            animateToKeyframeTwo()
+        }
+
+    }
+
+    fun animateToKeyframeTwo() {
+        val constraintSet = ConstraintSet()
+        constraintSet.load(this, R.layout.activity_main2)
+        TransitionManager.beginDelayedTransition(binding.root as ViewGroup?)
+        constraintSet.applyTo(constraintLayout)
     }
 }
